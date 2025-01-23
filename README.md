f081975016f909029cc86c69cf44dd8626206a9d
douglas85rj <douglas85rj@gmail.com>
2025-01-23 09:44:06 -0300

    A classe Game foi criada

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-Projeto criado em Java 21/Maven, com API rest e frontend. Foi utilizado Spring (Data JPA, Postgress, Spring web).
+## Sistema web Top Games
+
+Projeto criado em Java 21/Maven, com API rest e frontend. Foi utilizado Spring (Data JPA, Postgress, Spring web, H2).
 
 O objetivo é listar os jogos com nome e descrição.
 
@@ -10,4 +12,8 @@ Etapas do projeto:
 - GameMinDTO, GameRepository,
   GameService, GameController
 
+#### Arquitetura do sistema web
+
 ![1737593779152](image/README/1737593779152.png)
+
+![1737635045920](image/README/1737635045920.png)
---
+++ src/main/java/com/douglassouza/topgames/entities/Game.java
@@ -0,0 +1,114 @@
+package com.douglassouza.topgames.entities;
+
+public class Game {
+
+    private Long id;
+    private String titulo;
+    private Integer ano;
+    private String genero;
+    private String plataforma;
+    private String imgUrl;
+    private String descricaoCurta;
+    private String descricaoLonga;
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Game other = (Game) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getPlataforma() {
+        return plataforma;
+    }
+
+    public void setPlataforma(String plataforma) {
+        this.plataforma = plataforma;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getDescricaoCurta() {
+        return descricaoCurta;
+    }
+
+    public void setDescricaoCurta(String descricaoCurta) {
+        this.descricaoCurta = descricaoCurta;
+    }
+
+    public String getDescricaoLonga() {
+        return descricaoLonga;
+    }
+
+    public void setDescricaoLonga(String descricaoLonga) {
+        this.descricaoLonga = descricaoLonga;
+    }
+
+    public Game(Long id, String titulo, Integer ano, String genero, String plataforma, String imgUrl,
+                String descricaoCurta, String descricaoLonga) {
+        this.id = id;
+        this.titulo = titulo;
+        this.ano = ano;
+        this.genero = genero;
+        this.plataforma = plataforma;
+        this.imgUrl = imgUrl;
+        this.descricaoCurta = descricaoCurta;
+        this.descricaoLonga = descricaoLonga;
+    }
+}
