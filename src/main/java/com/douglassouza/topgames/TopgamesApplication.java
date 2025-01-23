a6a8963055f7f08744abf21ece6a7645979adb68
douglas85rj <douglas85rj@gmail.com>
2025-01-22 21:32:33 -0300

    first commit

---
+++ src/main/java/com/douglassouza/topgames/TopgamesApplication.java
@@ -0,0 +1,13 @@
+package com.douglassouza.topgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopgamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopgamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/douglassouza/topgames/TopgamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglassouza.topgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopgamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
